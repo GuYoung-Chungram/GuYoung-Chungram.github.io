c0b58ab0eb7363b5dcc3dd9d877321231234093a
GuYoung Jung <gyjung79@gmail.com>
2026-04-02 13:25:27 +0900

    Add config.js to .gitignore

--- play/streams/config.js
+++
@@ -1,9 +0,0 @@
-    var firebaseConfig = {
-      apiKey: "AIzaSyB_-pZHh5RN9ksAKqi6VOWgnW0kKIwfBAw",
-      authDomain: "blueblock-cdff7.firebaseapp.com",
-      databaseURL: "https://blueblock-cdff7-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "blueblock-cdff7",
-      storageBucket: "blueblock-cdff7.firebasestorage.app",
-      messagingSenderId: "273215826494",
-      appId: "1:273215826494:web:179e1fd7b5dbaaa2a5bf6f",
-    };
